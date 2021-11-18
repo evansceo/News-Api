dc9f82b75a1e2c7a9385dd5bebcd4dd6c028a808
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-19 00:47:34 +0300

    views.py file changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,17 @@ from . import main
 from ..request import get_articles,get_source
 
 
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_source = get_source()
+   
+    # print(popular_source)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular=popular_source)
+    
