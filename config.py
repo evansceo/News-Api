2832bfd8bbb004fc2c89155727bef683b9d4546f
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-19 00:38:57 +0300

    news-api in config file

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     '''
     NEWS_API_ARTICLES_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
     # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 class ProdConfig(Config):
