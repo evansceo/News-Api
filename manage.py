9aafa98bb2dd739b0560de22a7b4ce007f459220
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-19 00:05:49 +0300

    deployment to heruko

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
 from flask_script import Manager,Server
 
-app = create_app('development')
+app = create_app('production')
 
 manage = Manager(app)
 manage.add_command('server',Server)
