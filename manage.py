10407b0d9b2539adefa3fd2049d6e7f84fa4bf4b
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-19 00:34:12 +0300

    manage.py file

--- manage.py
+++ manage.py
@@ -6,12 +6,7 @@ app = create_app('production')
 manage = Manager(app)
 manage.add_command('server',Server)
 
-@manage.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 
 if __name__ == '__main__':
